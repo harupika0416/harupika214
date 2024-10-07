c44cfcd6866553effa51c823e30563434a1be622
harupika0416 <167415535+harupika0416@users.noreply.github.com>
2024-10-07 20:20:40 +0900

    Update script.js

--- blog/script.js
+++ blog/script.js
@@ -1,25 +1,8 @@
 document.cookie = "yuki=True; max-age=31536000; path=/"; // 自動承認
-localStorage.setItem('cookieAccepted', 'true'); // cookieが承認されたことを記録
+localStorage.setItem('followButton', 'true'); // ハルピカをフォローをおされたことを記録
 
 window.onload = function() {
-    const cookieAccepted = localStorage.getItem('cookieAccepted');
+    const cookieAccepted = localStorage.getItem('followButton');
     if (cookieAccepted !== 'true') {
         showCookieNotice();
     }
-};
-
-// ただし、cookieが承認された時に表示しないようにします。
-function showCookieNotice() {
-    const cookieNotice = document.getElementById('cookie-notice');
-    cookieNotice.style.opacity = '1';
-    cookieNotice.style.transform = 'translateY(0)';
-}
-
-function hideCookieNotice() {
-    const cookieNotice = document.getElementById('cookie-notice');
-    cookieNotice.style.opacity = '0';
-    cookieNotice.style.transform = 'translateY(20px)';
-    setTimeout(() => {
-        cookieNotice.style.display = 'none';
-    }, 500);
-}
