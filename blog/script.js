1539920b2adf796056f51bdf83839e1521af053b
harupika0416 <167415535+harupika0416@users.noreply.github.com>
2024-10-09 16:39:00 +0900

    r

--- blog/script.js
+++ blog/script.js
@@ -1,8 +1,10 @@
-document.cookie = "yuki=True; max-age=31536000; path=/"; // 自動承認
+@@ -2,7 +2,7 @@ document.cookie = "yuki=True; max-age=31536000; path=/"; // 自動承認
 localStorage.setItem('followButton', 'true'); // はるぴかをフォローをおされたことを記録
 
 window.onload = function() {
     const cookieAccepted = localStorage.getItem('followButton');
     if (cookieAccepted !== 'true') {
+    const followButton = localStorage.getItem('followButton');
+    if (followButton !== 'true') {
         showCookieNotice();
     }
